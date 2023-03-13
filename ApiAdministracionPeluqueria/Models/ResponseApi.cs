8931b32b68115eafd624f4749738ea39f88e83e3
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-03-13 17:45:09 -0300

    Se agrego la clase ResponseApi para utilizarla en la respuesta de los controladores

---
+++ ApiAdministracionPeluqueria/Models/ResponseApi.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiAdministracionPeluqueria.Models
+{
+    public class ResponseApi
+    {
+        public int Resultado { get; set; }
+
+        public string Mensaje { get; set; }
+
+        public object Data { get; set; }
+
+
+    }
+}
