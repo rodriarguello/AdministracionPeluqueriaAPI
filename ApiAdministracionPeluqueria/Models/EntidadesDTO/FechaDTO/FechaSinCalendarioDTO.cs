895f5ee90aa4034687a26fa644d9f8a17900bc28
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-04-10 12:03:27 -0300

    Se creo el DTO de fecha

---
+++ ApiAdministracionPeluqueria/Models/EntidadesDTO/FechaDTO/FechaSinCalendarioDTO.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ApiAdministracionPeluqueria.Models.EntidadesDTO.FechaDTO
+{
+    public class FechaSinCalendarioDTO
+    {
+        public int Id { get; set; }
+        
+        public DateTime Dia { get; set; }
+
+       
+    }
+}
