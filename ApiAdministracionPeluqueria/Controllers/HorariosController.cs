891c17080727e8fbd9cb78fc11ab5168c8d451d6
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-04-10 11:56:20 -0300

    Creacion de controlador horario y su DTO

---
+++ ApiAdministracionPeluqueria/Controllers/HorariosController.cs
@@ -0,0 +1,51 @@
+﻿using ApiAdministracionPeluqueria.Models;
+using ApiAdministracionPeluqueria.Models.EntidadesDTO.HorarioDTO;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAdministracionPeluqueria.Controllers
+{
+    [Route("api/horarios")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class HorariosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly ResponseApi responseApi;
+        private readonly IMapper mapper;
+
+        public HorariosController(ApplicationDbContext context, ResponseApi responseApi, IMapper mapper)
+        {
+            this.context = context;
+            this.responseApi = responseApi;
+            this.mapper = mapper;
+
+        }
+
+        [HttpGet("{idCalendario:int}")]
+        public async Task<ActionResult<ModeloRespuesta>> MostrarHorarios([FromRoute]int idCalendario)
+        {
+            try
+            {
+
+                var horarios = await context.Horarios.Where(horario => horario.IdCalendario == idCalendario).ToListAsync();
+                
+                if (horarios == null) return responseApi.respuestaError("No hay turnos para el id especificado");
+                
+                var horariosDTO = mapper.Map<List<HorarioSinCalendarioDTO>>(horarios);
+
+                return responseApi.respuestaExitosa(horarios);
+
+
+            }
+            catch (Exception ex)
+            {
+                return responseApi.respuestaError(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ ApiAdministracionPeluqueria/Models/EntidadesDTO/HorarioDTO/HorarioSinCalendarioDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiAdministracionPeluqueria.Models.EntidadesDTO.HorarioDTO
+{
+    public class HorarioSinCalendarioDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public TimeSpan Hora { get; set; }
+    }
+}
