d7e4426c8d2ce981ed1cb88bc51a1f8b42806d75
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-06-15 16:53:04 -0300

    Eliminando dependencias

--- ApiAdministracionPeluqueria/Controllers/CuentasController.cs
+++ ApiAdministracionPeluqueria/Controllers/CuentasController.cs
@@ -4,7 +4,6 @@ using ApiAdministracionPeluqueria.Models.EntidadesDTO.Autenticacion;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
