1c9c26e7e2bb9e43ab05824869aa0b159a5be822
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-07-07 19:58:43 -0300

    Creacion controlador caja

---
+++ ApiAdministracionPeluqueria/Controllers/CajaController.cs
@@ -0,0 +1,44 @@
+﻿using ApiAdministracionPeluqueria.Models;
+using ApiAdministracionPeluqueria.Models.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAdministracionPeluqueria.Controllers
+{
+    [Route("api/caja")]
+    [ApiController]
+    public class CajaController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public CajaController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("diario/{dia:int}")]
+        public async Task<ActionResult> MostrarIngresosPorDia()
+        {
+
+            return Ok();
+        }
+
+        [HttpGet("mensual/{mes:int}")]
+        public async Task<ActionResult> MostrarIngresosPorMes()
+        {
+
+            return Ok();
+        }
+
+        [HttpGet("anual/{anio:int}")]
+        public async Task<ActionResult> MostrarIngresosPorAnio()
+        {
+
+            return Ok();
+        }
+
+        
+
+    }
+}
