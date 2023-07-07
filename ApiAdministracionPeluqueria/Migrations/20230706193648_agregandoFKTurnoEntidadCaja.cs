94722550a756afa649cec3a22fafe1c05811a40a
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-07-07 20:05:50 -0300

    Modificacion entidad caja y sus migraciones

---
+++ ApiAdministracionPeluqueria/Migrations/20230706193648_agregandoFKTurnoEntidadCaja.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiAdministracionPeluqueria.Migrations
+{
+    /// <inheritdoc />
+    public partial class agregandoFKTurnoEntidadCaja : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TurnoId",
+                table: "Caja",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Caja_TurnoId",
+                table: "Caja",
+                column: "TurnoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Caja_Turnos_TurnoId",
+                table: "Caja",
+                column: "TurnoId",
+                principalTable: "Turnos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Caja_Turnos_TurnoId",
+                table: "Caja");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Caja_TurnoId",
+                table: "Caja");
+
+            migrationBuilder.DropColumn(
+                name: "TurnoId",
+                table: "Caja");
+        }
+    }
+}
