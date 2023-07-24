c6cb2b76f5fb81b66ff6c343fe09d0a1b08186bc
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-07-24 11:45:39 -0300

    Se agrego el campo fechaCreacion en entidad Mascota y Cliente y se crearon las migraciones

--- ApiAdministracionPeluqueria/Migrations/20230724144248_+fechaCreacionMascota_y_Cliente.Designer.cs
+++ ApiAdministracionPeluqueria/Migrations/20230724144248_+fechaCreacionMascota_y_Cliente.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ApiAdministracionPeluqueria.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiAdministracionPeluqueria.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230724144248_+fechaCreacionMascota_y_Cliente")]
+    partial class fechaCreacionMascota_y_Cliente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ApiAdministracionPeluqueria/Models/Entidades/Mascota.cs
+++ ApiAdministracionPeluqueria/Models/Entidades/Mascota.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiAdministracionPeluqueria.Models.Entidades
 {
@@ -33,5 +35,9 @@ namespace ApiAdministracionPeluqueria.Models.Entidades
         [Required]
 
         public string IdUsuario { get; set; }
+        
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime FechaCreacion { get; set; }
     }
 }
