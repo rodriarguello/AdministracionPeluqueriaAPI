4743bb9c04c1ef7f24b61d7e8445aa2cdfbe59d7
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-07-26 11:22:39 -0300

    Eliminacion de entidades Fecha y Horarios y migración a la base de datos

---
+++ ApiAdministracionPeluqueria/Migrations/20230726141318_eliminacionEntidadFechas_y_Horarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiAdministracionPeluqueria.Migrations
+{
+    /// <inheritdoc />
+    public partial class eliminacionEntidadFechas_y_Horarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Horario",
+                table: "Turnos",
+                type: "TIME(0)",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time(6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Horario",
+                table: "Turnos",
+                type: "time(6)",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "TIME(0)");
+        }
+    }
+}
