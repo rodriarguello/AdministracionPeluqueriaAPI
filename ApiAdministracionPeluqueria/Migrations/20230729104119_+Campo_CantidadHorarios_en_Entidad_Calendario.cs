fc3a3f65451fa9f8c4e6049fcaa9efcec8f04746
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-07-30 23:54:19 -0300

    Se agrego campo cantidadHorarios en entidad Calendario y se creo la migracion correspondiente

---
+++ ApiAdministracionPeluqueria/Migrations/20230729104119_+Campo_CantidadHorarios_en_Entidad_Calendario.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiAdministracionPeluqueria.Migrations
+{
+    /// <inheritdoc />
+    public partial class Campo_CantidadHorarios_en_Entidad_Calendario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CantidadHorarios",
+                table: "Calendarios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CantidadHorarios",
+                table: "Calendarios");
+        }
+    }
+}
