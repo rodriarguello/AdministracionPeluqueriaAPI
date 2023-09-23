9516403b1acd13d52753ff25e66d3cd23f693348
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-23 10:45:59 -0300

    Se creo la clase para inicializar la base de datos

---
+++ ApiAdministracionPeluqueria/Utilidades/DbInicializador.cs
@@ -0,0 +1,32 @@
+﻿using ApiAdministracionPeluqueria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAdministracionPeluqueria.Utilidades
+{
+    public class DbInicializador
+    {
+        private readonly ApplicationDbContext context;
+
+        public DbInicializador(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void InicializarDb()
+        {
+            try
+            {
+                if (context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    context.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
