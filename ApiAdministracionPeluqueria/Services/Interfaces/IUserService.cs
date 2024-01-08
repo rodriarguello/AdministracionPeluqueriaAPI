ebaccb6fed79e872b54050bb3c679052bb5fc3ce
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2024-01-08 18:17:26 -0300

    Creación e implementación de IUserService

---
+++ ApiAdministracionPeluqueria/Services/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using ApiAdministracionPeluqueria.Models.EntidadesDTO.UsuarioDTO;
+
+namespace ApiAdministracionPeluqueria.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UsuarioDTO> GetByEmailAsync (string email);
+
+        Task CreateAsync(CreacionUsuarioDTO creacionUsuarioDTO);
+    }
+}
---
+++ ApiAdministracionPeluqueria/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using ApiAdministracionPeluqueria.Exceptions;
+using ApiAdministracionPeluqueria.Models.Entidades;
+using ApiAdministracionPeluqueria.Models.EntidadesDTO.UsuarioDTO;
+using ApiAdministracionPeluqueria.Services.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+
+namespace ApiAdministracionPeluqueria.Services
+{
+    public class UserService:IUserService
+    {
+        private readonly UserManager<Usuario> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserService(UserManager<Usuario> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<UsuarioDTO> GetByEmailAsync(string email)
+        {
+            var usuario = await _userManager.FindByEmailAsync(email);
+
+            return _mapper.Map<UsuarioDTO>(usuario);
+        }
+
+        public async Task CreateAsync(CreacionUsuarioDTO creacionUsuarioDTO)
+        {
+            var usuario = _mapper.Map<Usuario>(creacionUsuarioDTO);
+
+            usuario.UserName = creacionUsuarioDTO.Email;
+
+            var resultado = await _userManager.CreateAsync(usuario, creacionUsuarioDTO.Password);
+
+            if (!resultado.Succeeded) throw new BadRequestException(string.Join("\n",resultado.Errors.Select(error => error.Description)));
+        }
+    }
+}
