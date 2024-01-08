ec33ca243975fc8729f57d4ff0ddd708092808e1
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2024-01-08 18:16:42 -0300

    Creación e implementación de ITokenService

---
+++ ApiAdministracionPeluqueria/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using ApiAdministracionPeluqueria.Models.EntidadesDTO.Autenticacion;
+
+namespace ApiAdministracionPeluqueria.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        RespuestaAutenticacion ConstruirToken(string email, string idUsuario);
+    }
+}
---
+++ ApiAdministracionPeluqueria/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using ApiAdministracionPeluqueria.Models.EntidadesDTO.Autenticacion;
+using ApiAdministracionPeluqueria.Services.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiAdministracionPeluqueria.Services
+{
+    public class TokenService:ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public RespuestaAutenticacion ConstruirToken(string email, string idUsuario)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim ("email", email),
+                new Claim ("id",idUsuario)
+            };
+
+
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["llaveJwt"]));
+
+            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+
+            var expiracion = DateTime.UtcNow.AddDays(1);
+
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiracion, signingCredentials: creds);
+
+            return new RespuestaAutenticacion()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiracion = expiracion
+            };
+
+
+        }
+    }
+}
