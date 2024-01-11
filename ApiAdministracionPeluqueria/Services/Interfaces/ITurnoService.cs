b89e2430464beef20456f81e421e32b371ae0363
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2024-01-11 18:14:04 -0300

    Creación e implementación de ITurnoService

---
+++ ApiAdministracionPeluqueria/Services/Interfaces/ITurnoService.cs
@@ -0,0 +1,19 @@
+﻿using ApiAdministracionPeluqueria.Models.EntidadesDTO.TurnoDTO;
+
+namespace ApiAdministracionPeluqueria.Services.Interfaces
+{
+    public interface ITurnoService
+    {
+        Task<List<TurnoDTO>> GetAllAsync(int idCalendario, string idUsuario);
+
+        Task<ResTurnosFiltrados> FiltrarPorFechasAsync(int calendarioId, DateTime fechaInicio, DateTime fechaFin, string idUsuario);
+
+        Task ReservarTurnoAsync(int idTurno, TurnoModificarDTO turnoModificarDTO, string idUsuario);
+
+        Task CancelarTurnoAsync(int idTurno, string idUsuario);
+
+        Task ModificarAsistenciaAsync(int idTurno, string idUsuario, bool asistio);
+
+        Task ModificarPrecioAsync(int idTurno, decimal nuevoPrecio, string idUsuario);
+    }
+}
